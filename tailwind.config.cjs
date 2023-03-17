cd4db9fe8b8ae260b26db890a077bcb7a37269db
luco17 <lucodonoghue@gmail.com>
2023-03-17 21:28:14 +0000

    updates.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        butty: "0 6px #d8b4fe",
+        buttyhover: "0 4px #d8b4fe",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
