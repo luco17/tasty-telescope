d7cf9c04a96a26c59c94b5ca783fbf8b272d2cf2
luco17 <lucodonoghue@gmail.com>
2023-04-04 15:55:47 +0100

    removing unused dependencies and adding alpine.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), preact()],
-  experimental: { assets: true },
+  integrations: [tailwind(), alpinejs()],
+  experimental: {
+    assets: true
+  }
 });
